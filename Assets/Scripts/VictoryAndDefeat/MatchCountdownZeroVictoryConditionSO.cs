af0ba11dde09d94768eb388b4f148bf923d35e7f
Daniel Jiménez Morales <64079694+DanielJimenezMorales@users.noreply.github.com>
2022-05-25 12:11:02 +0200

    Aniadido condicion de victoria maximo numero de bajas

--- Assets/Scripts/VictoryAndDefeat/MatchCountdownZeroVictoryConditionSO.cs
+++ Assets/Scripts/VictoryAndDefeat/MatchCountdownZeroVictoryConditionSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "ScriptableObjects/Victory Conditions/ Match Countdown Zero", fileName = "MatchCountdownZeroVictoryCondition")]
+[CreateAssetMenu(menuName = "ScriptableObjects/Victory Conditions/Match Countdown Zero", fileName = "MatchCountdownZeroVictoryCondition")]
 public class MatchCountdownZeroVictoryConditionSO : VictoryConditionSO
 {
     public override bool CheckCondition(int matchSecondsLeft)
